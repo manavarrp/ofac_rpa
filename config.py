03f4dc2171134e9f10d3bc4bdb76888d4523a787
Mario Navarro <manavarrp@gmail.com>
2026-02-26 22:31:03 -0500

    Se elimina config.py del repositorio

--- config.py
+++
@@ -1,3 +0,0 @@
-DATABASE_URL = "postgres://user4105:pruebauser41052023@rpa-prevalentware.c3rkad1ay1ao.us-east-1.rds.amazonaws.com:5432/prueba-tecnica"
-
-URL_OFAC = "https://sanctionssearch.ofac.treas.gov/"
